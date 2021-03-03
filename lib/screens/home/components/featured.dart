2b3734b695420e1ab6f1271d4d369be2a8289c5f
KChristelle <r.ayateke@alustudent.com>
2021-03-03 09:04:36 +0200

    Removed a container

--- lib/screens/home/components/featured.dart
+++ lib/screens/home/components/featured.dart
@@ -5,21 +5,19 @@ import 'package:plant_growth_tracking_app/resources/constants.dart';
 class FeaturedPlants extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SizedBox(
-        height: 300,
-        child: GridView.builder(
-          itemCount: plants.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: kDefaultPadding / 2,
-            mainAxisSpacing: kDefaultPadding / 2,
-            childAspectRatio: 0.7,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            return plants[index];
-          },
+    return SizedBox(
+      height: 300,
+      child: GridView.builder(
+        itemCount: plants.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: kDefaultPadding / 2,
+          // mainAxisSpacing: kDefaultPadding / 14,
+          childAspectRatio: 0.7,
         ),
+        itemBuilder: (BuildContext context, int index) {
+          return plants[index];
+        },
       ),
     );
   }
