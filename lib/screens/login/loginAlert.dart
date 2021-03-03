8fc9621c3929ac142a3449df1ba4f33d6a4f1ebb
joyce-njeri <43512265+joyce-njeri@users.noreply.github.com>
2021-03-03 18:31:58 +0200

    Added Login Alert

---
+++ lib/screens/login/loginAlert.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+showAlertDialog(BuildContext context) {
+  // set up the button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {},
+  );
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Account Creation"),
+    content: Text("Login Successful!"),
+    actions: [
+      okButton,
+    ],
+  );
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
