ebda7a41fefc9292bc0837024be1603ad8a438ea
joyce-njeri <43512265+joyce-njeri@users.noreply.github.com>
2021-04-23 21:41:32 +0200

    Update switch.dart

--- lib/screens/details/components/switch.dart
+++ lib/screens/details/components/switch.dart
@@ -4,11 +4,91 @@ import 'package:plant_growth_tracking_app/screens/details/fertilize.dart';
 import 'package:plant_growth_tracking_app/screens/details/sunlight.dart';
 import 'package:plant_growth_tracking_app/screens/details/watering.dart';
 
-class WGreenSwitch extends StatefulWidget {
-  final Function press;
-  final Function action;
+class MGreenSwitch extends StatefulWidget {
+  @override
+  _MGreenSwitchState createState() => _MGreenSwitchState();
+}
 
-  const WGreenSwitch({Key key, this.action, this.press}) : super(key: key);
+class _MGreenSwitchState extends State<MGreenSwitch> {
+  bool isSwitched = false;
+  void toggleSwitch(bool value) {
+    if (isSwitched == false) {
+      setState(() {
+        isSwitched = true;
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => DateTimePicker(),
+        //   ),
+        // );
+      });
+    } else {
+      setState(() {
+        isSwitched = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Transform.scale(
+          scale: 1,
+          child: Switch(
+            onChanged: toggleSwitch,
+            value: isSwitched,
+            activeColor: Colors.white,
+            activeTrackColor: lightGreen,
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey,
+          )),
+    ]);
+  }
+}
+
+class EGreenSwitch extends StatefulWidget {
+  @override
+  _EGreenSwitchState createState() => _EGreenSwitchState();
+}
+
+class _EGreenSwitchState extends State<EGreenSwitch> {
+  bool isSwitched = false;
+  void toggleSwitch(bool value) {
+    if (isSwitched == false) {
+      setState(() {
+        isSwitched = true;
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (context) => DateTimePicker(),
+        //   ),
+        // );
+      });
+    } else {
+      setState(() {
+        isSwitched = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Transform.scale(
+          scale: 1,
+          child: Switch(
+            onChanged: toggleSwitch,
+            value: isSwitched,
+            activeColor: Colors.white,
+            activeTrackColor: lightGreen,
+            inactiveThumbColor: Colors.white,
+            inactiveTrackColor: Colors.grey,
+          )),
+    ]);
+  }
+}
+
+class WGreenSwitch extends StatefulWidget {
   @override
   _WGreenSwitchState createState() => _WGreenSwitchState();
 }
@@ -51,10 +131,6 @@ class _WGreenSwitchState extends State<WGreenSwitch> {
 }
 
 class FGreenSwitch extends StatefulWidget {
-  final Function press;
-  final Function action;
-
-  const FGreenSwitch({Key key, this.action, this.press}) : super(key: key);
   @override
   _FGreenSwitchState createState() => _FGreenSwitchState();
 }
@@ -97,10 +173,6 @@ class _FGreenSwitchState extends State<FGreenSwitch> {
 }
 
 class SGreenSwitch extends StatefulWidget {
-  final Function press;
-  final Function action;
-
-  const SGreenSwitch({Key key, this.action, this.press}) : super(key: key);
   @override
   _SGreenSwitchState createState() => _SGreenSwitchState();
 }
